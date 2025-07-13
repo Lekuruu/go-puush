40ef9fb7830cc54d211f6bf17ea7f765cfad4d9c
Lekuru <contact@lekuru.xyz>
2025-07-13 23:19:32 +0200

    Add initial config system

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Lekuruu/go-puush
 
 go 1.24.5
+
+require github.com/kelseyhightower/envconfig v1.4.0
---
+++ internal/config.go
@@ -0,0 +1,28 @@
+package internal
+
+import "github.com/kelseyhightower/envconfig"
+
+type Config struct {
+	Api struct {
+		Host          string `envconfig:"API_HOST" default:"0.0.0.0"`
+		Port          int    `envconfig:"API_PORT" default:"8080"`
+		ClientVersion int    `envconfig:"API_CLIENT_VERSION" default:"93"`
+		ClientUrl     string `envconfig:"API_CLIENT_URL" default:"http://puush.me/dl/puush-win.zip"`
+	}
+	Database struct {
+		Path       string `envconfig:"DB_PATH" default:"/.data/puush.db"`
+		Migrations string `envconfig:"DB_MIGRATIONS" default:"./migrations"`
+	}
+	Storage struct {
+		Type string `envconfig:"STORAGE_TYPE" default:"local"`
+		Uri  string `envconfig:"STORAGE_URI" default:"/.data/"`
+	}
+}
+
+func Load() (*Config, error) {
+	var config Config
+	if err := envconfig.Process("", &config); err != nil {
+		return nil, err
+	}
+	return &config, nil
+}
