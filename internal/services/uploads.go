3ba473f54c001963dee38184615f3a4bd95d0ccd
Lekuru <contact@lekuru.xyz>
2025-07-14 20:15:09 +0200

    Add basic upload queries

--- internal/services/uploads.go
+++ internal/services/uploads.go
@@ -1,1 +1,59 @@
 package services
+
+import (
+	"github.com/Lekuruu/go-puush/internal/app"
+	"github.com/Lekuruu/go-puush/internal/database"
+)
+
+func CreateUpload(upload *database.Upload, state *app.State) error {
+	result := state.Database.Create(upload)
+
+	if result.Error != nil {
+		return result.Error
+	}
+
+	return nil
+}
+
+func FetchUploadById(id int, state *app.State, preload ...string) (*database.Upload, error) {
+	upload := &database.Upload{}
+	result := preloadQuery(state, preload).First(upload, id)
+
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return upload, nil
+}
+
+func FetchUploadByChecksum(checksum string, state *app.State, preload ...string) (*database.Upload, error) {
+	upload := &database.Upload{}
+	query := preloadQuery(state, preload).Where("checksum = ?", checksum)
+	result := query.First(upload)
+
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return upload, nil
+}
+
+func UpdateUpload(upload *database.Upload, state *app.State) error {
+	result := state.Database.Save(upload)
+
+	if result.Error != nil {
+		return result.Error
+	}
+
+	return nil
+}
+
+func DeleteUpload(upload *database.Upload, state *app.State) error {
+	result := state.Database.Delete(upload)
+
+	if result.Error != nil {
+		return result.Error
+	}
+
+	return nil
+}
