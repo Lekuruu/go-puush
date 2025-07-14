e8159418f3c3bb96563aeb87309597712e122483
Lekuru <contact@lekuru.xyz>
2025-07-14 17:56:25 +0200

    Add initial `/api/hist` request validation

--- api/history.go
+++ api/history.go
@@ -1,4 +1,18 @@
 package api
 
-// POST /api/hist
-func PuushHistory(ctx *Context) {}
+import "net/http"
+
+func PuushHistory(ctx *Context) {
+	user, err := UserAuthenticationFromContext(ctx)
+	if err != nil {
+		WritePuushError(ctx, -1, http.StatusUnauthorized)
+		return
+	}
+
+	if user == nil {
+		WritePuushError(ctx, -2, http.StatusInternalServerError)
+		return
+	}
+
+	// TODO: Retrieve uploads
+}
