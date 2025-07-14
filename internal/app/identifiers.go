8cd737403be1aa844e684e3999c61fb13796d49e
Lekuru <contact@lekuru.xyz>
2025-07-14 21:55:23 +0200

    Add utils for generating api keys & pool identifiers

---
+++ internal/app/identifiers.go
@@ -0,0 +1,26 @@
+package app
+
+import "math/rand"
+
+const Charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func randomString(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	result := make([]byte, length)
+	for i := range result {
+		result[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(result)
+}
+
+func GenerateApiKey() string {
+	return randomString(32)
+}
+
+func GeneratePoolPassword() string {
+	return randomString(32)
+}
+
+func GeneratePoolIdentifier() string {
+	return randomString(8)
+}
