baad2771276d48f877d753492f2596ade4b28d65
Lekuru <contact@lekuru.xyz>
2025-07-14 18:32:18 +0200

    Add dummy history response

--- api/history.go
+++ api/history.go
@@ -1,6 +1,18 @@
 package api
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/Lekuruu/go-puush/internal/database"
+)
+
+type HistoryResponse struct {
+	Uploads []*database.Upload
+}
+
+func (r *HistoryResponse) Serialize() []byte {
+	return []byte("0\n")
+}
 
 func PuushHistory(ctx *Context) {
 	user, err := UserAuthenticationFromContext(ctx)
@@ -14,5 +26,12 @@ func PuushHistory(ctx *Context) {
 		return
 	}
 
-	// TODO: Retrieve uploads
+	// TODO: Retrieve user uploads
+	history := &HistoryResponse{Uploads: []*database.Upload{}}
+	ctx.Response.WriteHeader(http.StatusOK)
+	_, err = ctx.Response.Write(history.Serialize())
+	if err != nil {
+		WritePuushError(ctx, -2, http.StatusInternalServerError)
+		return
+	}
 }
