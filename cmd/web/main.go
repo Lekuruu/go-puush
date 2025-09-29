7e06e13bf25ee64a96a28d0a20f915c5c37528f3
Lekuru <contact@lekuru.xyz>
2025-09-29 20:09:04 +0200

    Add about page

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,6 +9,7 @@ import (
 
 func InitializeRoutes(server *app.Server) {
 	server.Router.HandleFunc("/", server.ContextMiddleware(routes.Home)).Methods("GET")
+	server.Router.HandleFunc("/about", server.ContextMiddleware(routes.About)).Methods("GET")
 	server.Router.HandleFunc("/login", server.ContextMiddleware(routes.Login)).Methods("GET")
 	server.Router.HandleFunc("/account", server.ContextMiddleware(routes.Account)).Methods("GET")
 	server.Router.HandleFunc("/register", server.ContextMiddleware(routes.Register)).Methods("GET")
