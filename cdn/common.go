b7c4d6ecc94be1731d087428249f2ef2b79357de
Lekuru <contact@lekuru.xyz>
2025-10-07 15:22:38 +0200

    Add date, last modified & etag headers

--- cdn/common.go
+++ cdn/common.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/Lekuruu/go-puush/internal/app"
 	"github.com/Lekuruu/go-puush/internal/database"
@@ -25,6 +26,9 @@ func WriteUpload(ctx *app.Context, upload *database.Upload, data []byte) {
 	ctx.Response.Header().Set("Content-Type", upload.MimeType)
 	ctx.Response.Header().Set("Content-Length", strconv.Itoa(int(upload.Filesize)))
 	ctx.Response.Header().Set("Content-Disposition", fmt.Sprintf("filename=\"%s\"", upload.Filename))
+	ctx.Response.Header().Set("Last-Modified", upload.CreatedAt.Format(http.TimeFormat))
+	ctx.Response.Header().Set("Date", time.Now().Format(http.TimeFormat))
+	ctx.Response.Header().Set("ETag", fmt.Sprintf(`"%s"`, upload.Checksum))
 	ctx.Response.WriteHeader(200)
 	ctx.Response.Write(data)
 }
@@ -34,6 +38,10 @@ func WriteThumbnail(ctx *app.Context, upload *database.Upload, thumbnail []byte)
 	ctx.Response.Header().Set("Content-Type", http.DetectContentType(thumbnail))
 	ctx.Response.Header().Set("Content-Length", strconv.Itoa(len(thumbnail)))
 	ctx.Response.Header().Set("Content-Disposition", fmt.Sprintf("filename=\"%s\"", thumbnailFilename))
+	ctx.Response.Header().Set("Last-Modified", upload.CreatedAt.Format(http.TimeFormat))
+	ctx.Response.Header().Set("Date", time.Now().Format(http.TimeFormat))
+	// Thumbnails are not the original upload, so we don't use the same etag
+	ctx.Response.Header().Set("ETag", fmt.Sprintf(`"t%s"`, upload.Checksum))
 	ctx.Response.WriteHeader(200)
 	ctx.Response.Write(thumbnail)
 }
