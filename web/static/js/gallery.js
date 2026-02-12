ebcf59a24ebd1abc8f6d0c230de15c34fed58ac4
Lekuru <contact@lekuru.xyz>
2026-02-12 22:01:52 +0100

    Revert "Add support for video embeds in gallery"
    This reverts commit f12f0c7353a0a4471f74ab4e5288daf0e6e2a488.

--- web/static/js/gallery.js
+++ web/static/js/gallery.js
@@ -7,26 +7,7 @@
     base_url: "",
 
     buildViewURL: function(id) {
-      return galleries.base_url + galleries.items[id].id;
-    },
-
-    isVideo: function(id) {
-      var item = galleries.items[id];
-      if (!item) return false;
-
-      if (item.extension) {
-        var videoExts = ['mp4', 'webm', 'mov', 'ogv'];
-        return videoExts.indexOf(item.extension.toLowerCase()) !== -1;
-      }
-
-      if (item.type && item.type.indexOf('video/') === 0) {
-        return true;
-      }
-
-      var url = galleries.buildViewURL(id);
-      var ext = url.split('.').pop().toLowerCase().split('?')[0];
-      var videoExts = ['mp4', 'webm', 'mov', 'ogv'];
-      return videoExts.indexOf(ext) !== -1;
+      return galleries.base_url + galleries.items[id].id
     },
 
     refreshItems: function() {
@@ -38,32 +19,24 @@
     },
 
     resizeCanvas: function() {
-      var node = $('#gallery_image > a > img')[0] || $('#gallery_image > a > video')[0];
-      if (!node) return;
+      var node = $('#gallery_image > a > img')[0];
 
-      var nodeWidth, nodeHeight;
-
-      if (node.tagName === 'VIDEO') {
-        nodeWidth = node.videoWidth || node.offsetWidth;
-        nodeHeight = node.videoHeight || node.offsetHeight;
-      } else {
-        nodeWidth = node.naturalWidth || node.width;
-        nodeHeight = node.naturalHeight || node.height;
-      }
+      // This is so we can get the base dimensions of the image.
+      var image = new Image();
+      image.src = galleries.buildViewURL(galleries.selected);
 
       var max_height = w.innerHeight - 96;
       var max_width = Math.max(w.innerWidth - 150, 550);
 
-      if (nodeWidth > 0) {
-        var ar = nodeHeight / nodeWidth;
-        var scale = Math.min(1, (max_width / nodeWidth), (max_height / nodeHeight));
-        var new_width = nodeWidth * scale;
-        var new_height = nodeWidth * ar * scale;
-        node.height = new_height;
-        node.width = new_width;
-      }
+      var ar = image.height / image.width;
+      var scale = Math.min(1, (max_width/image.width), (max_height/image.height));
 
-      $(node).attr("style", "position:absolute;top:74px;left:10px;");
+      var new_width = image.width * scale;
+      var new_height = image.width * ar * scale;
+
+      node.height = new_height;
+      node.width  = new_width;
+      $(node).attr("style", "position:absolute;top:74px;left:10px;"); // Remove the temporary max-width and max-height
     },
 
     redrawSelected: function() {
@@ -73,24 +46,13 @@
       var max_height = w.innerHeight - 96;
       var max_width = Math.max(w.innerWidth - 150, 550);
 
-      if (galleries.isVideo(galleries.selected)) {
-        var videoUrl = galleries.buildViewURL(galleries.selected);
-        parent.append(
-          "<video style='max-width:" + max_width + "px;max-height:" + max_height + "px;' " +
-          "controls autoplay loop muted playsinline>" +
-          "<source src='" + videoUrl + "' type='video/mp4'>" +
-          "</video>"
-        );
-        $('#gallery_image > a > video').on('loadedmetadata', galleries.resizeCanvas);
-      } else {
-        parent.append("<img style='max-width:" + max_width + "px;max-height:" + max_height + "px;'/>");
-        $('#gallery_image > a > img').imagesLoaded(galleries.resizeCanvas);
-        parent.find('img')[0].src = galleries.buildViewURL(galleries.selected);
-      }
+      parent.append("<img style='max-width:"+max_width+"px;max-height:"+max_height+"px;'/>");
+      $('#gallery_image > a > img').imagesLoaded(galleries.resizeCanvas);
+      parent.find('img')[0].src = galleries.buildViewURL(galleries.selected);
     },
 
     redrawThumbs: function() {
-      $('#gallery_thumbs').fadeOut(function() {
+      $('#gallery_thumbs').fadeOut(function(){
         $('#gallery_thumbs').html("");
         $('#galleryThumbs').tmpl(galleries.items).appendTo('#gallery_thumbs');
         $('#gallery_thumbs').fadeIn(function() {
@@ -109,10 +71,10 @@
       galleries.selected = id;
 
       $('#gallery_thumbs > .puush_tile').removeClass('selected').addClass('unselected');
-      $('#thumb_' + id).removeClass('unselected').addClass('selected');
+      $('#thumb_'+id).removeClass('unselected').addClass('selected');
 
       if (keyboard) {
-        $("#gallery_sidebar").stop().scrollTo("#thumb_" + id, 500, { offset: -5 });
+        $("#gallery_sidebar").stop().scrollTo("#thumb_"+id, 500, { offset: -5 });
       }
 
       $('#gallery_image > a').attr('href', galleries.buildViewURL(galleries.selected));
@@ -126,26 +88,26 @@
       galleries.refreshItems();
 
       $(w).bind('resize', galleries.resizeCanvas);
-      $(document).keydown(function(e) {
-        switch (e.keyCode) {
+      $(document).keydown(function(e){
+        switch(e.keyCode){
           case 37: // left
           case 38: // up
             if (galleries.selected > 0) {
-              galleries.view((galleries.selected - 1), true);
+              galleries.view((galleries.selected-1), true);
               return false;
             }
-            break;
+          break;
 
           case 39: // right
           case 40: // down
-            if (galleries.selected < (galleries.item_count - 1)) {
-              galleries.view((galleries.selected + 1), true);
+            if (galleries.selected < (galleries.item_count-1)) {
+              galleries.view((galleries.selected+1), true);
               return false;
             }
-            break;
+          break;
         }
-      });
+      })
     },
-  };
+  }
   $(w.document).ready(galleries.init);
 })(window);
